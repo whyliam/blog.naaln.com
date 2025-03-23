70cf9dff801c81140dbaf961931889c3584504fc
whyliam <3387850+whyliam@users.noreply.github.com>
2025-03-23 22:43:46 +0800

    hexo-footnote

--- scripts/AddTail.js
+++ scripts/AddTail.js
@@ -1,21 +1,34 @@
-hexo.extend.filter.register('after_post_render', function(data){
+hexo.extend.filter.register("after_post_render", function (data) {
+  var before_content = "";
+  before_content +=
+    '<p class="cc">本文首发于<a href="https://blog.naaln.com">Why·Liam·Blog (https://blog.naaln.com)</a>，版权所有，侵权必究。</p>';
+  before_content +=
+    '<p class="cc">本文永久链接：<a href="' +
+    data.permalink +
+    '">' +
+    data.permalink +
+    "</a><p>";
 
-  var before_content = '';
-  before_content += '<p class="cc">本文首发于<a href="https://blog.naaln.com">Why·Liam·Blog (https://blog.naaln.com)</a>，版权所有，侵权必究。</p>';
-  before_content += '<p class="cc">本文永久链接：<a href="'+data.permalink+'">'+data.permalink+'</a><p>';
-  
-  var file_content = '';
-  file_content += '<p class="cc">The <a href="https://blog.naaln.com">Why·Liam·Blog</a> by <a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">Creative Commons BY-NC-ND 4.0 International License</a>.</p>';
-  file_content += '<p class="cc">由<a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a>创作并维护的<a href="https://blog.naaln.com">Why·Liam·Blog</a>采用创作共用<a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">保留署名-非商业-禁止演绎4.0国际许可证</a>。</p>';
-  file_content += '<p class="cc">本文首发于<a href="https://blog.naaln.com">Why·Liam·Blog (https://blog.naaln.com)</a>，版权所有，侵权必究。</p>';
-  file_content += '<p class="cc">本文永久链接：<a href="'+data.permalink+'">'+data.permalink+'</a><p>';
+  var file_content = "";
+  file_content +=
+    '<p class="cc">The <a href="https://blog.naaln.com">Why·Liam·Blog</a> by <a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">Creative Commons BY-NC-ND 4.0 International License</a>.</p>';
+  file_content +=
+    '<p class="cc">由<a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a>创作并维护的<a href="https://blog.naaln.com">Why·Liam·Blog</a>采用创作共用<a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">保留署名-非商业-禁止演绎4.0国际许可证</a>。</p>';
+  file_content +=
+    '<p class="cc">本文首发于<a href="https://blog.naaln.com">Why·Liam·Blog (https://blog.naaln.com)</a>，版权所有，侵权必究。</p>';
+  file_content +=
+    '<p class="cc">本文永久链接：<a href="' +
+    data.permalink +
+    '">' +
+    data.permalink +
+    "</a><p>";
 
   try {
     if (file_content && data.content.length > 50) {
       data.content = data.content + file_content;
     }
   } catch (err) {
-    if (err.code !== 'ENOENT') throw err;
+    if (err.code !== "ENOENT") throw err;
 
     // No process for ENOENT error
   }
--- scripts/ScrollTo.js
+++ scripts/ScrollTo.js
@@ -1,5 +1,9 @@
-'use strict';
+"use strict";
 
-hexo.extend.injector.register('head_end', function() {
-  return `<script>document.addEventListener('DOMContentLoaded',function(){if(window.innerWidth<=767){window.scrollTo({top:120,behavior:'smooth'});}});</script>`;
-}, 'post');
+hexo.extend.injector.register(
+  "head_end",
+  function () {
+    return '<script>requestAnimationFrame(()=>window.innerWidth<=767&&window.scrollTo({top:120,behavior:"smooth"}))</script>';
+  },
+  "post"
+);
