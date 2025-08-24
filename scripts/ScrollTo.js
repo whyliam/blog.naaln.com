b774e40d9dc4b43d427961692ad7e11bbff4e0b1
whyliam <3387850+whyliam@users.noreply.github.com>
2025-08-25 00:09:53 +0800

    seo

--- scripts/ScrollTo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-hexo.extend.injector.register(
-  "head_end",
-  function () {
-    return '<script>requestAnimationFrame(()=>window.innerWidth<=767&&window.scrollTo({top:120,behavior:"smooth"}))</script>';
-  },
-  "post"
-);
